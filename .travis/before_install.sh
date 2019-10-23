55716181db398c5a50df306f851fcf7637e49651
Joachim Metz <joachim.metz@gmail.com>
2019-10-23 14:27:03 -0700

    Applied updates

---
+++ .travis/before_install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Script to run before_install step on Travis-CI
+#
+# Version: 20190808
+
+# Exit on error.
+set -e;
+
+if test ${TARGET} = "docker";
+then
+	sudo apt-get update;
+	sudo apt-mark hold mysql-server-5.7 postgresql-9.4 postgresql-client-9.4 postgresql-9.5 postgresql-client-9.5 postgresql-9.6 postgresql-client-9.6 postgresql-10 postgresql-client-10;
+	sudo apt-get --fix-missing -o Dpkg::Options::="--force-confold" upgrade -y --allow-unauthenticated;
+
+	sudo apt-get install -y qemu-user-static;
+
+	docker run --rm --privileged multiarch/qemu-user-static --reset -p yes;
+
+elif test ${TRAVIS_OS_NAME} = "linux" || test ${TRAVIS_OS_NAME} = "linux-ppc64le";
+then
+	sudo apt-get update;
+	sudo apt-mark hold mysql-server-5.7 postgresql-9.4 postgresql-client-9.4 postgresql-9.5 postgresql-client-9.5 postgresql-9.6 postgresql-client-9.6 postgresql-10 postgresql-client-10;
+	sudo apt-get --fix-missing -o Dpkg::Options::="--force-confold" upgrade -y --allow-unauthenticated;
+
+	sudo apt-get install -y autopoint;
+
+elif test ${TRAVIS_OS_NAME} = "osx";
+then
+	brew update
+
+	brew install gettext gnu-sed;
+	brew link --force gettext;
+fi
+
