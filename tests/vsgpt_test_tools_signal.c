a3a1cf3e5410eba03cdcaafd7824582dd31965fa
Joachim Metz <joachim.metz@gmail.com>
2021-02-24 13:41:38 +0100

    Worked on tests

--- tests/vsgpt_test_tools_signal.c
+++ tests/vsgpt_test_tools_signal.c
@@ -33,13 +33,55 @@
 
 #include "../vsgpttools/vsgpttools_signal.h"
 
-void vsgpt_test_tools_signal_handler(
+void vsgpt_test_tools_signal_handler_function(
       vsgpttools_signal_t signal VSGPT_TEST_ATTRIBUTE_UNUSED )
 {
 	VSGPT_TEST_UNREFERENCED_PARAMETER( signal )
 }
 
-/* Tests the vsgpttools_signal_attach and function
+#if defined( WINAPI )
+
+/* Tests the vsgpttools_signal_handler function
+ * Returns 1 if successful or 0 if not
+ */
+int vsgpt_test_tools_signal_handler(
+     void )
+{
+	BOOL result = 0;
+
+	/* Test regular cases
+	 */
+	result = vsgpttools_signal_handler(
+	          CTRL_C_EVENT );
+
+	VSGPT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 (int) TRUE );
+
+	result = vsgpttools_signal_handler(
+	          CTRL_LOGOFF_EVENT );
+
+	VSGPT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 (int) FALSE );
+
+	return( 1 );
+
+on_error:
+	return( 0 );
+}
+
+#if defined( _MSC_VER )
+
+	/* TODO add tests for vsgpttools_signal_initialize_memory_debug */
+
+#endif /* defined( _MSC_VER ) */
+
+#endif /* defined( WINAPI ) */
+
+/* Tests the vsgpttools_signal_attach function
  * Returns 1 if successful or 0 if not
  */
 int vsgpt_test_tools_signal_attach(
@@ -48,8 +90,10 @@ int vsgpt_test_tools_signal_attach(
 	libcerror_error_t *error = NULL;
 	int result               = 0;
 
+	/* Test regular cases
+	 */
 	result = vsgpttools_signal_attach(
-	          vsgpt_test_tools_signal_handler,
+	          vsgpt_test_tools_signal_handler_function,
 	          &error );
 
 	VSGPT_TEST_ASSERT_EQUAL_INT(
@@ -60,6 +104,24 @@ int vsgpt_test_tools_signal_attach(
 	VSGPT_TEST_ASSERT_IS_NULL(
 	 "error",
 	 error );
+
+	/* Test error cases
+	 */
+	result = vsgpttools_signal_attach(
+	          NULL,
+	          &error );
+
+	VSGPT_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	VSGPT_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
 
 	return( 1 );
 
@@ -72,7 +134,7 @@ on_error:
 	return( 0 );
 }
 
-/* Tests the vsgpttools_signal_detach and function
+/* Tests the vsgpttools_signal_detach function
  * Returns 1 if successful or 0 if not
  */
 int vsgpt_test_tools_signal_detach(
@@ -81,6 +143,8 @@ int vsgpt_test_tools_signal_detach(
 	libcerror_error_t *error = NULL;
 	int result               = 0;
 
+	/* Test regular cases
+	 */
 	result = vsgpttools_signal_detach(
 	          &error );
 
@@ -121,13 +185,17 @@ int main(
 
 #if defined( WINAPI )
 
-	/* TODO add tests for vsgpttools_signal_handler */
-#endif
+	VSGPT_TEST_RUN(
+	 "vsgpttools_signal_handler",
+	 vsgpt_test_tools_signal_handler )
 
-#if defined( WINAPI ) && defined( _MSC_VER )
+#if defined( _MSC_VER )
 
 	/* TODO add tests for vsgpttools_signal_initialize_memory_debug */
-#endif
+
+#endif /* defined( _MSC_VER ) */
+
+#endif /* defined( WINAPI ) */
 
 	VSGPT_TEST_RUN(
 	 "vsgpttools_signal_attach",
