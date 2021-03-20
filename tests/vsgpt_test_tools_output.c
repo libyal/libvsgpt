cb72560f75f609517f969689c1251664350d2f0a
Joachim Metz <joachim.metz@gmail.com>
2021-03-20 09:50:44 +0100

    Worked on tests

--- tests/vsgpt_test_tools_output.c
+++ tests/vsgpt_test_tools_output.c
@@ -39,7 +39,7 @@
 
 #include "../vsgpttools/vsgpttools_output.h"
 
-/* Tests the vsgpttools_output_initialize and function
+/* Tests the vsgpttools_output_initialize function
  * Returns 1 if successful or 0 if not
  */
 int vsgpt_test_tools_output_initialize(
