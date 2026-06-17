4e2f3fb11ce252553275d948045e6246eceb74ba
Joachim Metz <joachim.metz@gmail.com>
2026-06-17 17:20:34 +0200

    Worked on tests and changes for CI tests

--- pyvsgpt/pyvsgpt_volume.c
+++ pyvsgpt/pyvsgpt_volume.c
@@ -418,7 +418,7 @@ PyObject *pyvsgpt_volume_open(
 	int result                   = 0;
 
 #if defined( HAVE_WIDE_SYSTEM_CHARACTER )
-	const wchar_t *filename_wide = NULL;
+	wchar_t *filename_wide       = NULL;
 #else
 	PyObject *utf8_string_object = NULL;
 #endif
