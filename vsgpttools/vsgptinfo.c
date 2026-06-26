4357381a4d2f60b6932b76e79426416d6cb1d546
Joachim Metz <joachim.metz@gmail.com>
2026-06-26 12:05:53 +0200

    Worked on options and manpages

--- vsgpttools/vsgptinfo.c
+++ vsgpttools/vsgptinfo.c
@@ -1,5 +1,5 @@
 /*
- * Shows information obtained from a GUID Partition Table (GPT) volume system
+ * Shows information obtained from a GUID Partition Table (GPT) volume system.
  *
  * Copyright (C) 2019-2026, Joachim Metz <joachim.metz@gmail.com>
  *
@@ -21,11 +21,8 @@
 
 #include <common.h>
 #include <file_stream.h>
-#include <memory.h>
 #include <system_string.h>
 #include <types.h>
-
-#include <stdio.h>
 
 #if defined( HAVE_FCNTL_H ) || defined( WINAPI )
 #include <fcntl.h>
@@ -55,27 +52,6 @@
 
 info_handle_t *vsgptinfo_info_handle = NULL;
 int vsgptinfo_abort                  = 0;
-
-/* Prints the executable usage information
- */
-void usage_fprint(
-      FILE *stream )
-{
-	if( stream == NULL )
-	{
-		return;
-	}
-	fprintf( stream, "Use vsgptinfo to determine information about a GUID\n"
-	                 "Partition Table (GPT) volume system.\n\n" );
-
-	fprintf( stream, "Usage: vsgptinfo [ -hvV ] source\n\n" );
-
-	fprintf( stream, "\tsource: the source file\n\n" );
-
-	fprintf( stream, "\t-h:     shows this help\n" );
-	fprintf( stream, "\t-v:     verbose output to stderr\n" );
-	fprintf( stream, "\t-V:     print version\n" );
-}
 
 /* Signal handler for vsgptinfo
  */
@@ -129,10 +105,22 @@ int wmain( int argc, wchar_t * const argv[] )
 int main( int argc, char * const argv[] )
 #endif
 {
-	libcerror_error_t *error   = NULL;
+	const char *description = \
+		"Use vsgptinfo to determine information about a GUID Partition Table (GPT) volume system.";
+
+	vsgpttools_option_t options[ ] = {
+		{ 'h', NULL, "shows this help" },
+		{ 'v', NULL, "verbose output to stderr" },
+		{ 'V', NULL, "print version" },
+		{ 0, "source", "the source image" },
+	};
+	system_character_t options_string[ 32 ];
+
+	libvsgpt_error_t *error    = NULL;
 	system_character_t *source = NULL;
 	char *program              = "vsgptinfo";
 	system_integer_t option    = 0;
+	int number_of_options      = (int) ( sizeof( options ) / sizeof( vsgpttools_option_t ) );
 	int verbose                = 0;
 
 #if defined( __MINGW32__ ) && defined( HAVE_MINGW_BINMODE )
@@ -170,10 +158,22 @@ int main( int argc, char * const argv[] )
 	 stdout,
 	 program );
 
+	if( vsgpttools_getopt_get_options_string(
+	     options,
+	     number_of_options,
+	     options_string,
+	     32 ) != 1 )
+	{
+		fprintf(
+		 stderr,
+		 "Unable to determine options string.\n" );
+
+		goto on_error;
+	}
 	while( ( option = vsgpttools_getopt(
 	                   argc,
 	                   argv,
-	                   _SYSTEM_STRING( "hvV" ) ) ) != (system_integer_t) -1 )
+	                   options_string ) ) != (system_integer_t) -1 )
 	{
 		switch( option )
 		{
@@ -184,14 +184,22 @@ int main( int argc, char * const argv[] )
 				 "Invalid argument: %" PRIs_SYSTEM "\n",
 				 argv[ optind - 1 ] );
 
-				usage_fprint(
-				 stdout );
+				vsgpttools_getopt_usage_fprint(
+				 stdout,
+				 program,
+				 description,
+				 options,
+				 number_of_options );
 
 				return( EXIT_FAILURE );
 
 			case (system_integer_t) 'h':
-				usage_fprint(
-				 stdout );
+				vsgpttools_getopt_usage_fprint(
+				 stdout,
+				 program,
+				 description,
+				 options,
+				 number_of_options );
 
 				return( EXIT_SUCCESS );
 
@@ -211,10 +219,14 @@ int main( int argc, char * const argv[] )
 	{
 		fprintf(
 		 stderr,
-		 "Missing source file.\n" );
+		 "Missing source image.\n" );
 
-		usage_fprint(
-		 stdout );
+		vsgpttools_getopt_usage_fprint(
+		 stdout,
+		 program,
+		 description,
+		 options,
+		 number_of_options );
 
 		return( EXIT_FAILURE );
 	}
@@ -248,8 +260,7 @@ int main( int argc, char * const argv[] )
 	{
 		fprintf(
 		 stderr,
-		 "Unable to open: %" PRIs_SYSTEM ".\n",
-		 source );
+		 "Unable to open source image.\n" );
 
 		goto on_error;
 	}
