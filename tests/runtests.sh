3a9daf097abf2d38dc8c3cf916f6ffc716065d58
Joachim Metz <joachim.metz@gmail.com>
2026-09-16 10:57:36 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libvsgpt/.libs/libvsgpt.1.dylib && test -f ./pyvsgpt/.libs/pyvsgpt.so
+if [ -f "${PWD}/libvsgpt/.libs/libvsgpt.1.dylib" ] && [ -f ./pyvsgpt/.libs/pyvsgpt.so ]
 then
-	install_name_tool -change /usr/local/lib/libvsgpt.1.dylib ${PWD}/libvsgpt/.libs/libvsgpt.1.dylib ./pyvsgpt/.libs/pyvsgpt.so
+    install_name_tool -change /usr/local/lib/libvsgpt.1.dylib "${PWD}/libvsgpt/.libs/libvsgpt.1.dylib" ./pyvsgpt/.libs/pyvsgpt.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
